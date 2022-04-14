7cb7ae8f25bf4b95d7ea159324b0f2b00fb050af
sitnichuk91 <sitnichuk91@yandex.ru>
2022-04-14 22:36:06 +0700

    Добавлены обработчики нажатий на цифровые кнопки. Создан класс Calculator. В нём, в методах будщие арифметические действия. Поле input для работы с введенным выражением пользователя. Сама вьюха input - EditText. Так мы можем, во-первых с клавиатуры телефона вводить значения. А во-вторых не надо сохранять для горизонтальной ориентации.

---
+++ app/src/main/java/com/sitnichuk/calculator/Calculator.java
@@ -0,0 +1,94 @@
+package com.sitnichuk.calculator;
+
+public class Calculator {
+    private static final String ONE = "1";
+    private static final String TWO = "2";
+    private static final String THREE = "3";
+    private static final String FOUR = "4";
+    private static final String FIVE = "5";
+    private static final String SIX = "6t";
+    private static final String SEVEN = "7";
+    private static final String EIGHT = "8";
+    private static final String NINE = "9";
+    private static final String ZERO = "0";
+    private static final String DOT = ".";
+
+    private String input;
+
+
+    public static String getONE() {
+        return ONE;
+    }
+
+    public static String getTWO() {
+        return TWO;
+    }
+
+    public static String getTHREE() {
+        return THREE;
+    }
+
+    public static String getFOUR() {
+        return FOUR;
+    }
+
+    public static String getFIVE() {
+        return FIVE;
+    }
+
+    public static String getSIX() {
+        return SIX;
+    }
+
+    public static String getSEVEN() {
+        return SEVEN;
+    }
+
+    public static String getEIGHT() {
+        return EIGHT;
+    }
+
+    public static String getNINE() {
+        return NINE;
+    }
+
+    public static String getZERO() {
+        return ZERO;
+    }
+
+    public static String getDOT() {
+        return DOT;
+    }
+
+    public String getInput() {
+        return input;
+    }
+
+    public void setInput(String input) {
+        this.input = input;
+    }
+
+    public void sum() {
+
+    }
+
+    public void substract() {
+
+    }
+
+    public void multiply() {
+
+    }
+
+    public void divide() {
+
+    }
+
+    public void equals() {
+
+    }
+
+    public void clear() {
+
+    }
+}
